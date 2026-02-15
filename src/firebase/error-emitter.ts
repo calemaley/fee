32d5afbeb4ffb0f878a2e3993f883d89434b1696
Caleb Fundi <calebfundi19@gmail.com>
2026-02-15 22:44:10 +0000

    it has deployed in netlify but other pages are not opening
    Uncaught Typ

--- src/firebase/error-emitter.ts
+++ src/firebase/error-emitter.ts
@@ -1,3 +1,29 @@
-import { EventEmitter } from 'events';
+/**
+ * @fileOverview A lightweight, browser-safe event emitter for handling Firebase errors centrally.
+ * This avoids dependencies on Node.js built-ins like 'events' which cause 'Illegal constructor' errors in some environments.
+ */
 
-export const errorEmitter = new EventEmitter();
+type Listener = (...args: any[]) => void;
+
+class SimpleEmitter {
+  private listeners: { [event: string]: Listener[] } = {};
+
+  on(event: string, listener: Listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  off(event: string, listener: Listener) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter(l => l !== listener);
+  }
+
+  emit(event: string, ...args: any[]) {
+    if (!this.listeners[event]) return;
+    this.listeners[event].forEach(l => l(...args));
+  }
+}
+
+export const errorEmitter = new SimpleEmitter();
