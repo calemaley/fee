f18f5c5b7f1af7b68d8d62067f6a78f41d1da523
Caleb Fundi <calebfundi19@gmail.com>
2026-02-15 21:57:32 +0000

    and I've also replaced the "Access Restricted" landing page with a beaut

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,5 @@ export const firebaseConfig = {
   projectId: "schoolsystem-cc030",
   storageBucket: "schoolsystem-cc030.firebasestorage.app",
   messagingSenderId: "466605153663",
-  appId: "1:466605153663:web:74f27b388534d1110cce4a",
-  measurementId: "G-JHKKWTD8SY"
+  appId: "1:466605153663:web:ff471ca9c499e40f0cce4a"
 };
